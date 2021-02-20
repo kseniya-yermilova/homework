8a6ec47446100879a57aaf2a3e7f0330bb90982b
kseniya <ksyuw@yahoo.com>
2021-02-20 16:21:03 +0200

    Handle exeption

--- homework_4_5/src/test/java/automationpractice/test/BaseTest.java
+++ homework_4_5/src/test/java/automationpractice/test/BaseTest.java
@@ -1,4 +1,5 @@
 package automationpractice.test;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
@@ -8,20 +9,25 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected static WebDriver driver = null;
+
     @BeforeClass
-    public static void setup(){
-        System.setProperty("webdriver.chrome.driver","/Users/kseniya/Documents/QAavto/chromdriver/chromedriver");
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "/Users/kseniya/Documents/QAavto/chromdriver/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
+
     @AfterClass
-    public static void teardown() throws InterruptedException{
-        Thread.sleep(2*1000);
-        if(driver != null){
-            driver.quit();
+    public static void teardown() {
+        try {
+            Thread.sleep(2 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        else driver.close();
+        if (driver != null) {
+            driver.quit();
+        } else driver.close();
     }
 }
